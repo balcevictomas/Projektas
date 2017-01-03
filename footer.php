c2e659da0d4ec1a07a82ed011a98ebd6667938de
Daniel Parfionov <daniel.parfionov@gmail.com>
2017-01-03 20:00:18 +0200

    New hope

---
+++ footer.php
@@ -0,0 +1,32 @@
+  <footer class="page-footer grey">
+    <section>
+  <ul id='services'>
+    <h2>Social</h2>
+    <li>
+      <div class='entypo-facebook'></div>
+      <span>Facebook</span>
+    </li>
+    <li>
+      <div class='entypo-twitter'></div>
+      <span>Twitter</span>
+    </li>
+    <li>
+      <div class='entypo-gplus'></div>
+      <span>Google Plus</span>
+    </li>
+    <li>
+      <div class='entypo-linkedin'></div>
+      <span>LinkedIn</span>
+    </li>
+  </ul>
+</section>
+  </footer>
+
+
+  <!--  Scripts-->
+
+<script src="js/init.js"></script>
+<script src="//tympanus.net/codrops/adpacks/demoad.js"></script>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,122 @@
+<?php include 'header.php'; ?>
+
+              
+<!--   INDEX PRADZIA   -->
+<!--   INDEX PRADZIA   --> 
+<!--   INDEX PRADZIA   --> 
+
+  <!--   MAIN IMAGE PRADZIA   -->    
+  <img id="mainimg" src="images/main_picture.png"> 
+  <!--   MAIN IMAGE PABAIGA   -->
+
+<!--   SEARCH'as - PRADZIA   -->          
+    <form onsubmit="submitFn(this, event);">
+      <div class="search-wrapper">
+        <div class="input-holder">
+          <input type="text" class="search-input" placeholder="Type to search" />
+          <button class="search-icon" onclick="searchToggle(this, event);"><span></span></button>
+            </div>
+              <span class="close" onclick="searchToggle(this, event);"></span>
+            <div class="result-container">
+        </div>
+      </div>
+    </form>
+<!--   SEARCH'as - PABAIGA   --> 
+
+        <!--   TOP 4 barai - keturi langai - PRADZIA  -->
+        <div class="row"> 
+        <!--   pirmas langas   -->
+          <div class="col s12 m6 l3">
+            <div class="card z-depth-3">
+              <div class="card-image">
+                <img src="images/main-bar-one-700-460.png">
+                <span class="card-title">Sky Bar</span>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        <!--   pirmo lango pabaiga   -->
+        <!--   antras langas   -->
+          <div class="col s12 m6 l3">
+            <div class="card z-depth-3">
+              <div class="card-image">
+                <img src="images/main-bar-two-700-460.png">
+                <span class="card-title">Card Title</span>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        <!--   antro lango pabaiga   -->
+        <!--   trecias langas   -->
+          <div class="col s12 m6 l3">
+            <div class="card z-depth-3">
+              <div class="card-image">
+                <img src="images/main-bar-three-700-460.png">
+                <span class="card-title">Card Title</span>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        <!--   trecio lango pabaiga   -->
+        <!--   ketvirtas langas   -->
+          <div class="col s12 m6 l3">
+            <div class="card z-depth-3">
+              <div class="card-image">
+                <img src="images/main-bar-four-700-460.png">
+                <span class="card-title">Card Title</span>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">This is a link</a>
+              </div>
+            </div>
+          </div>
+        <!--   ketvirto lango pabaiga   -->
+      </div>
+      <!--   TOP 4 barai - keturi langai - PABAIGA   -->
+      
+<!--   INDEX PABAIGA   --> 
+<!--   INDEX PABAIGA   --> 
+<!--   INDEX PABAIGA   --> 
+
+
+<div class="content">
+  <figure class="effect-winston">
+  <img src="http://www.thetemplebarpub.com/pics/homepage_mainpic1.jpg" alt="img30">
+      <figcaption>
+        <h2>Jolly <span>Winston</span></h2>
+          <p>
+            <a href="#"><i class="fa fa-fw fa-star-o"></i></a>
+            <a href="#"><i class="fa fa-fw fa-comments-o"></i></a>
+            <a href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
+          </p>
+      </figcaption>     
+  </figure>
+</div>
+      </div>
+ </div>
+ </div>
+</div>
+
+<?php include 'footer.php'; ?>
