87d5a42d4a5102ff74d147c07c972f65f6a3f310
Elvyra-PC <turfer1337@gmail.com>
2017-01-09 18:36:07 +0200

    MAP google
    msadsad

--- news.php
+++ news.php
@@ -29,7 +29,7 @@
 
       <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FDELFI.Lietuva%2F&width=67&layout=button&action=like&size=small&show_faces=true&share=false&height=65&appId" width="67" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
        <div>
-      <img class="map1" src="images/alausmap.jpg">
+      <iframe class="alausBibiotekaMAP" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d815.5374646806675!2d25.278122257787587!3d54.67991467345591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd94136df7a761%3A0xad95acf10f7fb9b4!2sAlaus+Biblioteka!5e0!3m2!1slt!2slt!4v1483728462779" width="400" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div> 
       </div>
       </div>
@@ -72,7 +72,7 @@
       <p>„Q‘Polo“ - tai pasilinksminimu vieta – restoranas, baras , terasa ir šokiu aikštele. Šioje vietoje kiekvienas vilnietis ir miesto svecias atras tai, ko geidžia jo širdis – nuo aistringu šokiu iki gurnamiškos virtuves.</p>
       <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FDELFI.Lietuva%2F&width=67&layout=button&action=like&size=small&show_faces=true&share=false&height=65&appId" width="67" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
        <div>
-      <img class="map2" src="images/qpolomap.jpg">
+      <iframe class="qpoloMAP" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2306.7002290923992!2d25.274239215624103!3d54.67970448028002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd9413229b256d%3A0x9047c6dd5fa03284!2sQ&#39;polo!5e0!3m2!1slt!2slt!4v1483900503388" width="400" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
     </div>
   </div>
@@ -102,7 +102,7 @@
       <p>„Niño“ tikrai išsiskirtinis baras tiek Vilniuje, tiek, tikriausiai, ir visoje Lietuvoje nuo interjero iki maisto. Siulome paragauti puikiu ispanišku tapas, iš kuriu gausybes tikrai gales išsirinkti net išrankiausi. Vakarais atmosfera užpildo gyvai grojantys DJ-jai, kurie nenustelbs Jusu pokalbiu prie vyno taures, kokteilio ar alaus, kuriu ivairove mes tikrai pasirupinome.</p>
       <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FDELFI.Lietuva%2F&width=67&layout=button&action=like&size=small&show_faces=true&share=false&height=65&appId" width="67" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
       <div>
-      <img class="map3" src="images/ninomap.jpg">
+      <iframe class="ninoMAP" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2306.6134361083655!2d25.283204807689582!3d54.681232030907196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd94143c8e5f5f%3A0xf5d33ac8e6b0f32f!2sNino+Tapas+%26+Vine!5e0!3m2!1slt!2slt!4v1483900850070" width="400" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
     </div>
   </div>
